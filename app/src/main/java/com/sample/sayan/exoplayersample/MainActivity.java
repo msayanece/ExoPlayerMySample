bcaab5450f785c4b425ed3290290338bf34ea657
sayan <anshu.p@encoders.co.in>
2018-07-25 18:09:08 +0530

    basic video show done

--- app/src/main/java/com/sample/sayan/exoplayersample/MainActivity.java
+++ app/src/main/java/com/sample/sayan/exoplayersample/MainActivity.java
@@ -8,8 +8,10 @@ import android.view.View;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -25,14 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
+        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter())));
         SimpleExoPlayerView playerView = findViewById(R.id.playerView);
+        playerView.requestFocus();
         playerView.setPlayer(player);
 
         DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         DefaultDataSourceFactory mediaDataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
         DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
 
-        MediaSource mediaSource = new DashMediaSource(Uri.parse(URL), mediaDataSourceFactory, null, null);
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
+                mediaDataSourceFactory, extractorsFactory, null, null);
+        player.prepare(mediaSource);
     }
 }
