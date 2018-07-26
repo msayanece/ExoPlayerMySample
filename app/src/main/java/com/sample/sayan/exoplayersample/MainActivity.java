fa3610a5d43a2a0b9d6d0d8011b24fd219450f7e
sayan <anshu.p@encoders.co.in>
2018-07-26 16:19:15 +0530

    change to a generic class

--- app/src/main/java/com/sample/sayan/exoplayersample/MainActivity.java
+++ app/src/main/java/com/sample/sayan/exoplayersample/MainActivity.java
@@ -1,52 +1,22 @@
 package com.sample.sayan.exoplayersample;
-
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.dash.DashMediaSource;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.TransferListener;
-import com.google.android.exoplayer2.util.Util;
-
-import java.io.IOException;
+import com.sample.sayan.exoplayersample.exoplayer.GenericExoPlayer;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String URL = "http://underconstruction.co.in/eve2/upload/post/15_17_26_tomjerry.mp4";
+    private static final String URL_MP4 = "http://underconstruction.co.in/eve2/upload/post/15_17_26_tomjerry.mp4";
+    private static final String URL_MP3 = "http://underconstruction.co.in/eve2/upload/drive/FileImg1532430621.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter())));
-        SimpleExoPlayerView playerView = findViewById(R.id.playerView);
-        playerView.requestFocus();
-        playerView.setPlayer(player);
+    }
 
-        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        DefaultDataSourceFactory mediaDataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
-        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-
-//        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
-//                mediaDataSourceFactory, extractorsFactory, null, null);
-//
-        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://underconstruction.co.in/eve2/upload/drive/FileImg1532430621.mp3"),
-                mediaDataSourceFactory, extractorsFactory, null, null);
-//        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
-//                mediaDataSourceFactory,null, null);
-        player.prepare(mediaSource);
+    public void onClickButton(View view) {
+        GenericExoPlayer.showMediaDefault(this, URL_MP3, GenericExoPlayer.PLAYER_TYPE_DEFAULT);
     }
 }
---
+++ app/src/main/java/com/sample/sayan/exoplayersample/exoplayer/GenericExoPlayer.java
@@ -0,0 +1,76 @@
+package com.sample.sayan.exoplayersample.exoplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
+import com.sample.sayan.exoplayersample.R;
+
+public class GenericExoPlayer {
+
+    public static final int PLAYER_TYPE_DEFAULT = 1;
+
+    public static void showMediaDefault(Activity activity, String mediaUrl, int playerType) {
+        Intent intent = new Intent(activity, GenericExoPlayerActivity.class);
+        intent.putExtra("type", playerType);
+        intent.putExtra("mediaUrl", mediaUrl);
+        activity.startActivity(intent);
+    }
+
+    public static class GenericExoPlayerActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_generic_exo_player);
+            SimpleExoPlayerView playerView = findViewById(R.id.playerView);
+            int type = getIntent().getIntExtra("type", 1);
+            String mediaUrl = getIntent().getStringExtra("mediaUrl");
+            switch (type) {
+                case PLAYER_TYPE_DEFAULT:
+                    showDefaultPlayer(playerView, mediaUrl);
+                    break;
+                default:
+                    showDefaultPlayer(playerView, mediaUrl);
+                    break;
+            }
+
+//        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
+//                mediaDataSourceFactory, extractorsFactory, null, null);
+//
+//        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
+//                mediaDataSourceFactory,null, null);
+        }
+
+        private void showDefaultPlayer(SimpleExoPlayerView playerView, String mediaUrl){
+            SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(new DefaultBandwidthMeter())));
+            playerView.requestFocus();
+            playerView.setPlayer(player);
+
+            DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            DefaultDataSourceFactory mediaDataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "exoPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
+            DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            MediaSource mediaSource = new ExtractorMediaSource(Uri.parse(mediaUrl),
+                    mediaDataSourceFactory, extractorsFactory, null, null);
+
+            player.prepare(mediaSource);
+        }
+    }
+}
