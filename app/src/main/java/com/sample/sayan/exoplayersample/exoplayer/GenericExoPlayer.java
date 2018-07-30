195a5b73659187659a40bd079a1fedadb2e0c421
sayan <anshu.p@encoders.co.in>
2018-07-30 15:15:52 +0530

    add some design changes

--- app/src/main/java/com/sample/sayan/exoplayersample/exoplayer/GenericExoPlayer.java
+++ app/src/main/java/com/sample/sayan/exoplayersample/exoplayer/GenericExoPlayer.java
@@ -6,15 +6,24 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
@@ -42,12 +51,14 @@ public class GenericExoPlayer {
         private int type;
         private SimpleExoPlayerView playerView;
         private long contentPosition;
+        private ProgressBar progressBar;
 
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_generic_exo_player);
             playerView = findViewById(R.id.playerView);
+            progressBar = findViewById(R.id.progressBar);
             type = getIntent().getIntExtra("type", 1);
             mediaUrl = getIntent().getStringExtra("mediaUrl");
             initializePlayer();
@@ -56,7 +67,7 @@ public class GenericExoPlayer {
 //
         }
 
-        private void initializePlayer(){
+        private void initializePlayer() {
             switch (type) {
                 case PLAYER_TYPE_DEFAULT:
                     showDefaultPlayer(playerView, mediaUrl);
@@ -68,6 +79,74 @@ public class GenericExoPlayer {
                     showDefaultPlayer(playerView, mediaUrl);
                     break;
             }
+            addListenerToExoplayer();
+        }
+
+        private void addListenerToExoplayer() {
+            player.addListener(new Player.EventListener() {
+                @Override
+                public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+                }
+
+                @Override
+                public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+                }
+
+                @Override
+                public void onLoadingChanged(boolean isLoading) {
+
+                }
+
+                @Override
+                public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                    switch (playbackState) {
+                        case ExoPlayer.STATE_IDLE:
+                            progressBar.setVisibility(View.INVISIBLE);
+                            break;
+                        case ExoPlayer.STATE_BUFFERING:
+                            progressBar.setVisibility(View.VISIBLE);
+                            break;
+                        case ExoPlayer.STATE_READY:
+                            progressBar.setVisibility(View.INVISIBLE);
+                            break;
+                        case ExoPlayer.STATE_ENDED:
+                            progressBar.setVisibility(View.INVISIBLE);
+                            break;
+                    }
+                }
+
+                @Override
+                public void onRepeatModeChanged(int repeatMode) {
+
+                }
+
+                @Override
+                public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+                }
+
+                @Override
+                public void onPlayerError(ExoPlaybackException error) {
+
+                }
+
+                @Override
+                public void onPositionDiscontinuity(int reason) {
+
+                }
+
+                @Override
+                public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+                }
+
+                @Override
+                public void onSeekProcessed() {
+
+                }
+            });
         }
 
         private void showDefaultPlayer(SimpleExoPlayerView playerView, String mediaUrl) {
@@ -83,7 +162,7 @@ public class GenericExoPlayer {
                     mediaDataSourceFactory, extractorsFactory, null, null);
 
             player.prepare(mediaSource);
-            if (contentPosition != 0){
+            if (contentPosition != 0) {
                 player.seekTo(contentPosition);
             }
         }
@@ -101,7 +180,7 @@ public class GenericExoPlayer {
                     mediaDataSourceFactory, null, null);
 
             player.prepare(mediaSource);
-            if (contentPosition != 0){
+            if (contentPosition != 0) {
                 player.seekTo(contentPosition);
             }
         }
